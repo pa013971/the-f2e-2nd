814a30b1ec475a8112c2e6dbdccd1150cd71baad
Alan <alan@advmeds.com>
2021-01-15 18:56:03 +0800

    first commit

---
+++ src/views/FreeCell.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="freecell">
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueScreenSize from 'vue-screen-size'
+Vue.use(VueScreenSize)
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+  body {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .freecell {
+    position: relative;
+    margin: auto;
+    width: 100%;
+    height: 900px;
+    background-color: pink;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <router-link to="/">首頁</router-link> |
+      <router-link to="/freecell">新接龍</router-link>
+    </div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
